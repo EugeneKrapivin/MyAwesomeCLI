ef9296a71c7c21b613935baad5c900f8e6de24c5
Eugene Krapivin <eugene.krapivin@sap.com>
2023-03-06 20:37:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using MyAwesomeCLI;
+
+using Spectre.Console.Cli;
+
+var services = new ServiceCollection();
+
+services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
+var registrar = new TypeRegistrar(services);
+
+var app = new CommandApp(registrar);
+
+app.Configure(app =>
+{
+    app.AddBranch("context", context =>
+    {
+        context.AddCommand<CreateContextCommand>("create")
+            .WithDescription("this commands create a new context for our awesome app")
+            .WithExample(new[] { "--name", "test-context", "--user-key", "test-user", "--user-secret", "test-secret" });
+    });
+});
+
+await app.RunAsync(args);
